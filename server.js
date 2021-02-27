7d778ff9e7852919f4241ae2471609539564e934
Ryan Fanelli <illenafnayr@gmail.com>
2021-02-27 18:05:26 -0500

    url typo fix!

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config()
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 const corsOptions = {
-    origin: "https://ryanfanelli.dev"
+    origin: "https://ryanfanelli.dev/"
 };
 // Middleware
 app.use(cors(corsOptions));
